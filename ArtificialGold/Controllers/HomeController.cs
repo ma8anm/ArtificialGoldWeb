491846fc5aa1621b4c271d1a7e1bb8010a5aefae
Andrew Metcalfe <ametcalfe@opentable.com>
2013-01-17 16:45:35 +0000

    hardcoded controller constructor to avoid IOC woes

--- ArtificialGold/Controllers/HomeController.cs
+++ ArtificialGold/Controllers/HomeController.cs
@@ -2,12 +2,18 @@
 
 using ArtificialGold.Models.ViewModels;
 using ArtificialGold.Services;
+using ArtificialGold.Services.Phrases;
 
 namespace ArtificialGold.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IHomeViewModelRetriever _homeViewModelRetriever;
+
+        public HomeController():this(new HomeViewModelRetriever(new PhrasesRetriever()))
+        {
+            
+        }
 
         public HomeController(IHomeViewModelRetriever homeViewModelRetriever)
         {
